2b7b52e432b77024c4a4a1de9b58d96089af6d00
Rahul Kaushik <66063169+kaushikrahul08@users.noreply.github.com>
2023-07-28 12:11:51 -0400

    Update prometheus.sh

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -7,17 +7,20 @@ date
 curl google.com
 
 echo " Removing any exiting prometheus file "
-rm -Rrf /tmp/prometheus-2.20.0.linux-amd64*
+sudo rm -Rrf /tmp/prometheus-2.20.0.linux-amd64*
+sudo rm -Rrf /etc/prometheus
+sudo rm -Rrf /var/lib/prometheus
 
 ##downloading Prometheus binaries
 echo " downloading Prometheus binaries >>>>>>>>>>>>>>>"
 cd /tmp/ && wget https://github.com/prometheus/prometheus/releases/download/v2.20.0/prometheus-2.20.0.linux-amd64.tar.gz 
 
 useradd --no-create-home -s /bin/false prometheus
-mkdir /etc/prometheus
-mkdir /var/lib/prometheus
-chown prometheus:prometheus /etc/prometheus && chown prometheus:prometheus /var/lib/prometheus
-chmod 775 /etc/prometheus
+sudo mkdir -p /etc/prometheus/consoles
+sudo mkdir /etc/prometheus/console_libraries
+sudo mkdir /var/lib/prometheus
+sudo chown prometheus:prometheus /etc/prometheus && chown prometheus:prometheus /var/lib/prometheus
+sudo chmod 775 /etc/prometheus
 
 pwd
 
@@ -31,17 +34,17 @@ fi
 #unziping  the file
 tar -zxvf prometheus-2.20.0.linux-amd64.tar.gz
 
-cd /tmp/prometheus-2.20.0.linux-amd64/
-cp prometheus.yml /etc/prometheus/
-cp prometheus /usr/local/bin
-cp promtool /usr/local/bin
+sudo cd /tmp/prometheus-2.20.0.linux-amd64/
+sudo cp /tmp/prometheus-2.20.0.linux-amd64/prometheus.yml /etc/prometheus/
+sudo cp /tmp/prometheus-2.20.0.linux-amd64/prometheus /usr/local/bin
+sudo cp /tmp/prometheus-2.20.0.linux-amd64/promtool /usr/local/bin
 
 #optional step , 
-firewall-cmd --add-port=9090/tcp --permanent
-firewall-cmd --reload
+sudo firewall-cmd --add-port=9090/tcp --permanent
+sudo firewall-cmd --reload
 
 #copy prometheus executables to prom folder 
-cp -pr root/prometheus.service /etc/systemd/system/prometheus.service
+sudo cp -pr /home/$USER/prometheus.service /etc/systemd/system/prometheus.service
 
 #starting prometheus
 sudo systemctl daemon-reload
